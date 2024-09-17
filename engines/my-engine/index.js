52ec17cf962ab8d1a9e9003a06bbef5425dd0b4a
Liam Floyd <lfloyd@alarm.com>
2024-09-17 14:45:09 -0500

    Update engine references

--- engines/my-engine/index.js
+++ engines/my-engine/index.js
@@ -1,9 +1,25 @@
 'use strict';
 
-module.exports = {
-  name: require('./package').name,
+// eslint-disable-next-line n/no-unpublished-require, n/no-unpublished-require
+const EngineAddon = require('ember-engines/lib/engine-addon');
 
-  options: {
-    'ember-cli-babel': { enableTypeScriptTransform: true },
-  },
-};
+module.exports = EngineAddon.extend({
+    name: require('./package').name,
+
+    lazyLoading: true,
+
+    // Allows you to symlink the addon
+    isDevelopingAddon: () => false,
+
+    // Ensure SASS compilation.
+    included: function (app) {
+        this._super.included.apply(this, arguments);
+
+        // Allows the app babel config to overwrite properties of this addon babel config.
+        this.options.babel = Object.assign({}, this.options.babel, app.options.babel);
+        // Enable TS transpilation via Babel
+        this.options['ember-cli-babel'] = {
+            enableTypeScriptTransform: true
+        };
+    }
+});
